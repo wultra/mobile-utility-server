a35dce29cc478e6a326ee0ef25bc1c8b1ffdfb48
Petr Dvorak <petr@wultra.com>
2023-03-01 19:21:59 +0100

    Liquibase: Use YAML format insted of XML

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-liquibase --headless=true --log-level=INFO --changeLogFile=$LB_HOME/db/changelog/changelog.xml \
+liquibase --headless=true --log-level=INFO --changeLogFile=$LB_HOME/db/changelog/changelog.yaml \
   --username=$MOBILE_UTILITY_SERVER_DATASOURCE_USERNAME \
   --password=$MOBILE_UTILITY_SERVER_DATASOURCE_PASSWORD \
   --url=$MOBILE_UTILITY_SERVER_DATASOURCE_URL \
