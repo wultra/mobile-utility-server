4a1bb541b48cb2f49de2cabb2c1e707a8f67c07f
Petr Dvorak <petr@wultra.com>
2023-03-02 09:42:24 +0100

    Revert XML for Liquibase, optimize Docker build

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-liquibase --headless=true --log-level=INFO --changeLogFile=$LB_HOME/db/changelog/changelog.yaml \
+liquibase --headless=true --log-level=INFO --changeLogFile=$LB_HOME/db/changelog/changelog.xml \
   --username=$MOBILE_UTILITY_SERVER_DATASOURCE_USERNAME \
   --password=$MOBILE_UTILITY_SERVER_DATASOURCE_PASSWORD \
   --url=$MOBILE_UTILITY_SERVER_DATASOURCE_URL \
