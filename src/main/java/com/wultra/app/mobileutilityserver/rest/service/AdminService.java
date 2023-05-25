6b2f53b86c2b1d49fe2089e2b57aaa01b4037aa1
Lubos Racansky <lubos.racansky@gmail.com>
2023-05-25 09:18:02 +0200

    Fix typo

--- src/main/java/com/wultra/app/mobileutilityserver/rest/service/AdminService.java
+++ src/main/java/com/wultra/app/mobileutilityserver/rest/service/AdminService.java
@@ -199,7 +199,7 @@ public class AdminService {
         final PEMParser pemParser = new PEMParser(new StringReader(pem));
         final Object pemInfo = pemParser.readObject();
         if (pemInfo == null) {
-            throw new IOException("PerParser read null, appName: " + appName);
+            throw new IOException("PemParser read null, appName: " + appName);
         }
         pemParser.close();
         final X509CertificateHolder x509Cert = (X509CertificateHolder) pemInfo;
