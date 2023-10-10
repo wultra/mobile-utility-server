ce627887489412a2f22d1b3efc1065d2354a0ea8
Lubos Racansky <lubos.racansky@gmail.com>
2023-10-10 12:24:47 +0200

    Limit migration to mobile_app and certificate only

--- docs/sql/oracle/1.5.x-migration.sql
+++ docs/sql/oracle/1.5.x-migration.sql
@@ -4,15 +4,6 @@
 -- For table mobile_app
 INSERT INTO mus_mobile_app SELECT * FROM ssl_mobile_app;
 
--- For table mobile_domain
-INSERT INTO mus_mobile_domain SELECT * FROM ssl_mobile_domain;
-
--- For table user
-INSERT INTO mus_user SELECT * FROM ssl_user;
-
--- For table user_authority
-INSERT INTO mus_user_authority SELECT * FROM ssl_user_authority;
-
 -- For table certificate
 INSERT INTO mus_certificate SELECT * FROM ssl_certificate;
 
@@ -21,14 +12,8 @@ INSERT INTO mus_certificate SELECT * FROM ssl_certificate;
 -- Drop old tables
 DROP TABLE ssl_certificate CASCADE CONSTRAINTS;
 DROP TABLE ssl_mobile_app CASCADE CONSTRAINTS;
-DROP TABLE ssl_mobile_domain CASCADE CONSTRAINTS;
-DROP TABLE ssl_user CASCADE CONSTRAINTS;
-DROP TABLE ssl_user_authority CASCADE CONSTRAINTS;
 
 -- Drop old sequences
 DROP SEQUENCE ssl_certificate_seq;
 DROP SEQUENCE ssl_mobile_app_seq;
-DROP SEQUENCE ssl_mobile_domain_seq;
-DROP SEQUENCE ssl_user_authority_seq;
-DROP SEQUENCE ssl_user_seq;
 
--- docs/sql/postgresql/1.5.x-migration.sql
+++ docs/sql/postgresql/1.5.x-migration.sql
@@ -4,15 +4,6 @@
 -- For table mobile_app
 INSERT INTO mus_mobile_app SELECT * FROM ssl_mobile_app;
 
--- For table mobile_domain
-INSERT INTO mus_mobile_domain SELECT * FROM ssl_mobile_domain;
-
--- For table user
-INSERT INTO mus_user SELECT * FROM ssl_user;
-
--- For table user_authority
-INSERT INTO mus_user_authority SELECT * FROM ssl_user_authority;
-
 -- For table certificate
 INSERT INTO mus_certificate SELECT * FROM ssl_certificate;
 
@@ -21,13 +12,7 @@ INSERT INTO mus_certificate SELECT * FROM ssl_certificate;
 -- Drop old tables
 DROP TABLE ssl_certificate CASCADE;
 DROP TABLE ssl_mobile_app CASCADE;
-DROP TABLE ssl_mobile_domain CASCADE;
-DROP TABLE ssl_user CASCADE;
-DROP TABLE ssl_user_authority CASCADE;
 
 -- Drop old sequences
 DROP SEQUENCE ssl_certificate_seq CASCADE;
 DROP SEQUENCE ssl_mobile_app_seq CASCADE;
-DROP SEQUENCE ssl_mobile_domain_seq CASCADE;
-DROP SEQUENCE ssl_user_authority_seq CASCADE;
-DROP SEQUENCE ssl_user_seq CASCADE;
