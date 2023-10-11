b8d2c25ddb7e5fd778bc25dc9e3f4e77befd1b37
Lubos Racansky <lubos.racansky@gmail.com>
2023-10-11 13:42:51 +0200

    Fix #137: Error in after.sql
    Use the original table names from 1.1.0
    - mobile_app
    - mobile_ssl_pinning

--- docs/sql/oracle/1.5.x-migration.sql
+++ docs/sql/oracle/1.5.x-migration.sql
@@ -2,18 +2,18 @@
 -- TABLES
 
 -- For table mobile_app
-INSERT INTO mus_mobile_app SELECT * FROM ssl_mobile_app;
+INSERT INTO mus_mobile_app SELECT * FROM mobile_app;
 
 -- For table certificate
-INSERT INTO mus_certificate SELECT * FROM ssl_certificate;
+INSERT INTO mus_certificate SELECT * FROM mobile_ssl_pinning;
 
 
 
 -- Drop old tables
-DROP TABLE ssl_certificate CASCADE CONSTRAINTS;
-DROP TABLE ssl_mobile_app CASCADE CONSTRAINTS;
+DROP TABLE mobile_ssl_pinning CASCADE CONSTRAINTS;
+DROP TABLE mobile_app CASCADE CONSTRAINTS;
 
 -- Drop old sequences
-DROP SEQUENCE ssl_certificate_seq;
-DROP SEQUENCE ssl_mobile_app_seq;
+DROP SEQUENCE mobile_ssl_pinning_seq;
+DROP SEQUENCE mobile_app_seq;
 
--- docs/sql/postgresql/1.5.x-migration.sql
+++ docs/sql/postgresql/1.5.x-migration.sql
@@ -2,17 +2,17 @@
 -- TABLES
 
 -- For table mobile_app
-INSERT INTO mus_mobile_app SELECT * FROM ssl_mobile_app;
+INSERT INTO mus_mobile_app SELECT * FROM mobile_app;
 
 -- For table certificate
-INSERT INTO mus_certificate SELECT * FROM ssl_certificate;
+INSERT INTO mus_certificate SELECT * FROM mobile_ssl_pinning;
 
 
 
 -- Drop old tables
-DROP TABLE ssl_certificate CASCADE;
-DROP TABLE ssl_mobile_app CASCADE;
+DROP TABLE mobile_ssl_pinning CASCADE;
+DROP TABLE mobile_app CASCADE;
 
 -- Drop old sequences
-DROP SEQUENCE ssl_certificate_seq CASCADE;
-DROP SEQUENCE ssl_mobile_app_seq CASCADE;
+DROP SEQUENCE mobile_ssl_pinning_seq CASCADE;
+DROP SEQUENCE mobile_app_seq CASCADE;
