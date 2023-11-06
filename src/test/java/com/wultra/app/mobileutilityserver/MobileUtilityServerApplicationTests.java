eec45e66f422c4e09eab33a63cf1d10723577c33
Jan Pešek <jan.pesek@wultra.com>
2023-11-06 10:43:59 +0100

    Fix #152: HHH90000025: PostgreSQLDialect does not need to be specified explicitly

--- src/test/java/com/wultra/app/mobileutilityserver/MobileUtilityServerApplicationTests.java
+++ src/test/java/com/wultra/app/mobileutilityserver/MobileUtilityServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.wultra.app.mobileutilityserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = MobileUtilityServerApplicationTests.class)
+@ActiveProfiles("test")
 class MobileUtilityServerApplicationTests {
 
     @Test
