e49842393ee768ec3aba8ce76814177ae93d8171
Filip Bursik <filip.bursik@wultra.com>
2025-12-19 23:08:13 +0200

    Refactor application properties and Dockerfile for environment variable support

--- docker/database/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-if [ "${LQ_ENABLED}" = true ]; then
-  liquibase --headless=true --log-level=INFO --changeLogFile=$LB_HOME/db/changelog/db.changelog-master.xml \
-    --username=$MOBILE_UTILITY_SERVER_DATASOURCE_USERNAME \
-    --password=$MOBILE_UTILITY_SERVER_DATASOURCE_PASSWORD \
-    --url=$MOBILE_UTILITY_SERVER_DATASOURCE_URL \
-    update
-fi
