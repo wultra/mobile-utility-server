a36e3c0b7d5f56a955533752d34a331708addbc4
Filip Bursik <filip.bursik@wultra.com>
2026-01-12 13:30:52 +0200

    Update application properties and Docker entrypoint script for consistent environment variable usage

--- docker-init/docker-entrypoint.sh
+++ docker-init/docker-entrypoint.sh
@@ -5,9 +5,9 @@ KEEP_RUNNING_PORT=${KEEP_RUNNING_PORT:-666}
 
 liquibase --headless=true --log-level=INFO \
     --changeLogFile="changelog/db.changelog-master.xml" \
-    --username="${POWERAUTH_CLOUD_DATASOURCE_USERNAME:-}" \
-    --password="${POWERAUTH_CLOUD_DATASOURCE_PASSWORD:-}" \
-    --url="${POWERAUTH_CLOUD_DATASOURCE_URL}" \
+    --username="${MOBILE_UTILITY_SERVER_DATASOURCE_USERNAME:-}" \
+    --password="${MOBILE_UTILITY_SERVER_DATASOURCE_PASSWORD:-}" \
+    --url="${MOBILE_UTILITY_SERVER_DATASOURCE_URL}" \
     update
 
 if [[ -n "${KEEP_RUNNING}" && "${KEEP_RUNNING}" == "true" ]]; then
